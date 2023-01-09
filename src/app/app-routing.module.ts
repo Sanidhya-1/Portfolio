7b5a857bcb0eb71eceeaeb5c83f3c6e48c180033
Sanidhya kumar mishra <sanidhyakrmishra@gmail.com>
2023-01-09 12:01:42 +0530

    improved header and wrote about experience and project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExperienceandprojectComponent } from './main/experienceandproject/experienceandproject.component';
 import { HomeComponent } from './main/home/home.component';
+import { SkillsComponent } from './main/skills/skills.component';
 
 const routes: Routes = [{component:ExperienceandprojectComponent,path:"experience"},
-{component:HomeComponent,path:""}];
+{component:HomeComponent,path:""},
+{component:SkillsComponent,path:"skills"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ExperienceandprojectComponent } from './main/experienceandproject/experienceandproject.component';
 import { HeaderComponent } from './main/header/header.component';
 import { HomeComponent } from './main/home/home.component';
+import { SkillsComponent } from './main/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExperienceandprojectComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
